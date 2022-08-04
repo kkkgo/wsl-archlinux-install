6d54f9e58565089dda967305b1ef7531d4e93aba
kissshot <4530897+kkkgo@users.noreply.github.com>
2022-08-04 21:25:57 +0800

    update kernel

--- Install.bat
+++ Install.bat
@@ -2,13 +2,25 @@
 echo !NEED: Microsoft Windows [10.0.18362+]
 echo Your version:
 ver
-start update_kernel.bat
-timeout 3
-timeout 2
-timeout 1
-wsl --unregister ArchLinux
+
+@echo off
+:: Get Administrator Rights
+set _Args=%*
+if "%~1" NEQ "" (
+  set _Args=%_Args:"=%
+)
+fltmc 1>nul 2>nul || (
+  cd /d "%~dp0"
+  cmd /u /c echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~dp0"" && ""%~dpnx0"" ""%_Args%""", "", "runas", 1 > "%temp%\GetAdmin.vbs"
+  "%temp%\GetAdmin.vbs"
+  del /f /q "%temp%\GetAdmin.vbs" 1>nul 2>nul
+  exit
+)
+
+cd /d %~sdp0
 wsl_update_x64.msi /q
 wsl --update
+wsl --unregister ArchLinux
 wsl --set-default-version 2
 echo Install rootfs...
 ren archlinux-bootstrap*x86_64.tar.gz archlinux-bootstrap-x86_64.tar.gz
